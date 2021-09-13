14bef350228e859b10239119d913b8b3a6b69c29
Bo Goedhart <b.f.goedhart@icloud.com>
2021-09-13 11:58:53 +0000

    klasovertypen

--- script.js
+++ script.js
@@ -48,3 +48,34 @@ function draw() {
 
 
 }
+
+// code van Cam
+
+
+// globale variabelen
+var bal = new Bal(50, 100);
+
+/**
+ * setup
+ * de code in deze functie wordt één keer uitgevoerd door
+ * de p5 library, zodra het spel geladen is in de browser
+ */
+function setup() {
+  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  createCanvas(1280, 720);
+
+}
+
+
+/**
+ * draw
+ * de code in deze functie wordt meerdere keren per seconde
+ * uitgevoerd door de p5 library, nadat de setup functie klaar is
+ */
+function draw() {
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+
+  bal.show();
+  bal.update();
+}
